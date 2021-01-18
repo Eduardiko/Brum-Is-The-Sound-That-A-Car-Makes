37046b45a5d07685b128ebb99fb8d353814f9667
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2021-01-18 13:16:47 +0100

    Adding sensors structure

---
+++ ModuleMap.cpp
@@ -0,0 +1,150 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleMap.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+
+ModuleMap::ModuleMap(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	checkpointCounter = 0;
+}
+
+ModuleMap::~ModuleMap()
+{}
+
+// Load assets
+bool ModuleMap::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	//Camera init
+	
+
+	//Audio Load
+	winSound = App->audio->LoadFx("sfx/WinSoundEffect.wav");
+	
+	App->audio->PlayMusic("music/DejaVu.ogg");
+
+	//Create map
+	/*FirstPhaseObjects();
+	SecondPhaseObjects();
+	ThirdPhaseObjects();
+	LastPhaseObjects();
+	CreateSensors();
+	CreateConstrains();*/
+
+	return ret;
+}
+
+// Load assets
+bool ModuleMap::CleanUp()
+{
+	LOG("Unloading Intro scene");
+	map_objects.clear();
+	map_sensors.clear();
+	obs_bodys.Clear();
+	obs_primitives.Clear();
+	return true;
+}
+
+// Update
+update_status ModuleMap::Update(float dt)
+{
+	Plane p(0, 1, 0, 0);
+	p.axis = true;
+	p.Render();
+
+	
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleMap::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+{
+}
+
+PhysBody3D* ModuleMap::CreateRectangle(vec3 position, vec4 rotation, vec3 size, Color s_color, float mass)
+{
+
+	Cube* object = new Cube();
+
+	object->SetPos(position.x, position.y, position.z);
+	object->size = size;
+	object->color = s_color;
+	object->SetRotation(rotation.x, { rotation.y, rotation.z, rotation.w });
+
+	map_objects.add(object);
+	return App->physics->AddBody(*object, mass);
+}
+
+PhysBody3D* ModuleMap::CreateCylinder(vec3 position, vec4 rotation, float radius, float height, Color s_color, float mass)
+{
+	Cylinder* object = new Cylinder();
+
+	object->SetPos(position.x, position.y, position.z);
+	object->color = s_color;
+	object->SetRotation(rotation.x, { rotation.y, rotation.z, rotation.w });
+
+	object->height = height;
+	object->radius = radius;
+
+	map_objects.add(object);
+	return App->physics->AddBody(*object, mass);
+}
+
+void ModuleMap::CreateSensor(vec3 position, vec4 rotation, vec3 size, Color s_color, SensorType s_type, vec3 mod, vec4 target_rotation)
+{
+	/*Cube* object = new Cube();
+
+	object->SetPos(position.x, position.y, position.z);
+	object->size = size;
+	object->color = s_color;
+	object->SetRotation(rotation.x, { rotation.y, rotation.z, rotation.w });
+
+	if (s_type == SensorType::CHECKPOINT) 
+	{
+		checkPoints[checkpointCounter] = map_sensors.add(App->physics->AddSensor(*object, mod, s_type, target_rotation))->data;
+		checkpointCounter++;
+	}
+	else
+	{
+		map_sensors.add(App->physics->AddSensor(*object, mod, s_type, target_rotation));
+	}*/
+}
+
+void ModuleMap::FirstPhaseObjects()
+{
+	//------------------------ROAD LIMITS---------------------------------------
+	//Left side ------------------------------------------------
+	
+	
+}
+
+void ModuleMap::SecondPhaseObjects()
+{
+	
+}
+
+void ModuleMap::ThirdPhaseObjects()
+{
+	
+}
+
+void ModuleMap::LastPhaseObjects()
+{
+	
+}
+
+void ModuleMap::CreateSensors()
+{
+	
+
+	
+
+}
+
+void ModuleMap::CreateConstrains()
+{
+	
+}
--- PhysBody3D.h
+++ PhysBody3D.h
@@ -2,10 +2,20 @@
 #define __PhysBody3D_H__
 
 #include "p2List.h"
+#include "glmath.h"
 #include "Primitive.h"
 
 class btRigidBody;
 class Module;
+
+enum SensorType {
+
+	BOOSTER,
+	FINISH,
+	START,
+	CHECKPOINT,
+	RESPAWN
+};
 
 // =================================================
 struct PhysBody3D
@@ -26,6 +36,24 @@ private:
 
 public:
 	p2List<Module*> collision_listeners;
+};
+
+struct PhysSensor3D : public PhysBody3D
+{
+
+public:
+	PhysSensor3D(btRigidBody* body, SensorType s_type);
+	~PhysSensor3D();
+
+	void SetAsSensor(bool is_sensor);
+
+public:
+	SensorType type;
+	vec3 gravityMod;
+	vec4 targetRot;
+	bool isEnabled;
+
+	Primitive* lights[2];
 };
 
 #endif // __PhysBody3D_H__
